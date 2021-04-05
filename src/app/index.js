d60ee31868bde9a2ce6278bee2ea637ecb20926e
Niten Nashiki <n.nashiki.work@gmail.com>
2021-04-05 11:08:25 +0900

    update: css の変更が分かりやすいように変更

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,5 @@
 const button1 = document.getElementById("test_button1");
 button1.addEventListener("click", (e) => {
   document.getElementById(e.target.id).innerText = "ボタン押下後";
+  document.getElementById(e.target.id).className = "font-semibold rounded-lg shadow-md text-white bg-red-400";
 });
